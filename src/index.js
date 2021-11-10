1d146887d24124ed5003a150aff48797975c7dbc
Kenneth Kim <kkm2059@gmail.com>
2021-11-09 20:08:54 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+// let img = document.createElement("img")
+// img.src = imgUrl
+
+// let dogImage = document.getElementById("dog-image-container")
+// dogImage.appendChild(img)
+dogImageContainer = document.getElementById("dog-image-container")
+
+function addImage() {
+    const p = document.createElement('p')
+    p.id = `${message}`
+
+    dogImageContainer.appendChild(image)
+}
+
+fetch(imgUrl)
+.then(resp => resp.json())
+.then(image => image.forEach(addImage)
+).catch(console.log(error))
